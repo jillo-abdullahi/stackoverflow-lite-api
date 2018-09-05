b9a8dc6a409a558534c8e20ae93fa146e1b786df
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-09-05 19:41:50 +0300

    [Feature #160248419] Add view function for user to post question

--- app/v1/views/questions.py
+++ app/v1/views/questions.py
@@ -2,48 +2,54 @@
 
 from flask import Blueprint, jsonify, request
 
+from app.utilities import check_keys, check_empty_dict
+from app.v1.models import Question
+
 questions = Blueprint("questions_blueprint", __name__,
-                      url_prefix="/stackoverflowlite/api/v1/questions")
+                      url_prefix="/stackoverflowlite/api/v1")
+
+question_instance = Question()
 
 
-@questions.route('/', methods=['POST'])
+@questions.route('/questions', methods=['POST'])
 def post_question():
     """method to post a new question"""
     question_info = request.get_json()
 
-    return jsonify({"Question": question_info})
+    # Check that all fields have been provided
+    if check_keys(question_info, 2):
+        response = jsonify(
+            {"Error": "Please provide question title and description"})
+        return response, 400
+
+    # Check for empty values
+    if check_empty_dict(question_info):
+        response = jsonify({"Error": "Fields must not be empty"})
+        return response, 400
+
+    # Check if question's already been asked
+    existing_questions = question_instance.questions
+    for id in existing_questions:
+        if question_info['title'].lower() == existing_questions[id]['title'].lower():
+            response = jsonify(
+                {"Error": "That question has already been asked"})
+            return response, 400
+    # Add question
+    question_instance.save(question_info)
+    response = jsonify(
+        {"message": "Question added successfully", "Questions": existing_questions})
+    return response, 201
 
 
-@questions.route('/', methods=['GET'])
+@questions.route('/questions', methods=['GET'])
 def get_questions():
     """method to fetch all questions"""
 
     return jsonify({"Questions": "All questions"})
 
 
-@questions.route('/<question_id>', methods=['GET'])
+@questions.route('questions/<question_id>', methods=['GET'])
 def fetch_specific_question(question_id):
     """Method to fetch a specific question using id"""
 
     return jsonify({"Question": "Question matching id"})
-
-
-@questions.route('/<question_id>', methods=['DELETE'])
-def delete_specific_question(question_id):
-    """Method to delete a specific question"""
-
-    return jsonify({"message": "Question deleted if id matches"})
-
-
-@questions.route('/<question_id>/answers', methods=['POST'])
-def post_answer_to_question(question_id):
-    """Method to post answer to a specific question"""
-
-    return jsonify({"message": "Success if question exists"})
-
-
-@questions.route('/<question_id>/answers/<answer_id>', methods=['PUT'])
-def accept_answer(question_id, answer_id):
-    """Method to accept an answer to a specific question"""
-
-    return jsonify({"message": "Success if question and answer found"})
