bb0544e393b5075dbbc01b9e5878f85f9d6a5bf2
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-09-05 19:39:56 +0300

    [Feature #160248419] Add model for questions

--- app/v1/models.py
+++ app/v1/models.py
@@ -9,3 +9,15 @@ class User(object):
         """Method to create user and add to user dictionary"""
         id = str(len(self.users) + 1)
         self.users[str(id)] = user_details
+
+
+class Question(object):
+    """Class for questions"""
+
+    def __init__(self):
+        self.questions = {}
+
+    def save(self, question_details):
+        """Method to add questions to questions dictionary"""
+        id = str(len(self.questions) + 1)
+        self.questions[str(id)] = question_details
